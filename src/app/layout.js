214fa87879173e6e84a96bad75a9af3e5b7fa549
rzznoxy <rezajelek2@gmail.com>
2023-12-18 19:25:49 +0700

    apakah udah bener?

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
-import Providers from "./Providers";
 import NavbarGlobals from "@/modules/NavbarGlobal";
+import Providers from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
